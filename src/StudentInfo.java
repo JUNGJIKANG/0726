1954fe25330f98e8dbc14c6e76df74923318f7ac
JUNGJIKANG <wlrkd3229@naver.com>
2022-07-26 10:37:54 +0900

    Create '22.07.26.(화) Project

---
+++ src/StudentInfo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; //Scanner를 쓰려면 이 문장이 필요
+public class StudentInfo {
+  public static void main(String[] args) { //프로그램의 시작과 끝
+	Scanner sc = new Scanner(System.in);
+	System.out.print("오늘 날짜를 입력하세요 : "); //문자는 반드시""안에 작성할 것.
+	String Today = sc.nextLine(); //스캐너를 통해서 키보드로 한 줄을 다 읽으라는 의미의 sc, =은 좌측의 의미를 우측에 대입한다는 의미
+	
+	System.out.print("학생의 이름을 입력하세요 : ");
+	String Name = sc.nextLine();
+	
+	System.out.print("학생의 학번을 입력하세요 : ");
+	String Hakbun = sc.nextLine();
+	
+	System.out.println("날짜 : " + Today);
+	System.out.println("이름 : " + Name);
+	System.out.println("학번 : " + Hakbun);
+}
+}
